7413680b8bd1631c09b7a4b49ebd55a99423a72b
Nguyen Quang Huy <patrick204nqh@gmail.com>
2021-03-15 08:38:53 +0700

    [update] config main erp app

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # Dir.glob(Rails.root.join('engines').to_s + "/*") do |d|
+  #   eg = d.split(/[\/\\]/).last
+	# 	if eg != "backend" and Erp::Backend.available?(eg)
+	# 		mount "Erp::#{eg.camelize}::Engine".constantize => "/", as: 'erp_' + eg
+	# 	end
+  # end
+  mount Erp::Backend::Engine => "/", as: "erp"
 end
